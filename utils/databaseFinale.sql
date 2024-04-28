d9c21914502dd813ded915c81aca7446534610d9
kurtounet <bossut.anthony@gmail.com>
2024-04-28 08:09:10 +0200

    Upudate

--- utils/databaseFinale.sql
+++ utils/databaseFinale.sql
@@ -15,11 +15,11 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
--- Listage de la structure de la base pour dauphineexamor
-CREATE DATABASE IF NOT EXISTS `dauphineexamor` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `dauphineexamor`;
+-- Listage de la structure de la base pour dauphineexam
+CREATE DATABASE IF NOT EXISTS `dauphineexam` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `dauphineexam`;
 
--- Listage de la structure de table dauphineexamor. annonce
+-- Listage de la structure de table dauphineexam. annonce
 CREATE TABLE IF NOT EXISTS `annonce` (
   `id` int NOT NULL AUTO_INCREMENT,
   `imageUrl` varchar(250) DEFAULT NULL,
@@ -29,17 +29,18 @@ CREATE TABLE IF NOT EXISTS `annonce` (
   `datePublication` datetime NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `annonce_id_uindex` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=64 DEFAULT CHARSET=latin1;
 
--- Listage des données de la table dauphineexamor.annonce : ~5 rows (environ)
+-- Listage des données de la table dauphineexam.annonce : ~6 rows (environ)
 INSERT INTO `annonce` (`id`, `imageUrl`, `contenu`, `titre`, `auteur`, `datePublication`) VALUES
 	(54, 'https://localhost/bossutanthonydauphine/assets/uploads/lotus.png', 'Nouvelle annonce placeholder image', 'Nouvelle annonce placeholder image 9:25 -> Modifier à 9:39 ', 'placeholder', '2024-04-27 11:36:10'),
 	(57, 'https://localhost/bossutanthonydauphine/assets/uploads/lotus.png', 'Nouvelle annonce -> Modification image + titre + contenu + auteur', 'Nouvelle annonce -> Modification image + titre + contenu + auteur 2', 'Lotus', '2024-04-27 09:32:53'),
 	(58, 'https://localhost/bossutanthonydauphine/assets/uploads/oiseau.png', 'Nouvelle annonce lotus -> Modification image + titre + contenu + auteur ->oiseau', 'Nouvelle annonce lotus -> Modification image + titre + contenu + auteur ->oiseau', 'auteur ->oiseau', '2024-04-27 09:35:56'),
 	(59, 'https://placehold.co/400', 'Nouvelle annonce : date  27/04/24 9:36', 'Nouvelle annonce : date 27/04/24 9:37 -> modifier à  9:37', 'Anthony', '2024-04-27 09:38:43'),
-	(60, 'https://localhost/bossutanthonydauphine/assets/uploads/oiseau.png', 'Ma Nouvelle annonce placehold -> oiseau', 'Ma Nouvelle annonce placehold -> Modif tout ', 'Moi -> oiseau', '2024-04-27 12:36:09');
+	(60, 'https://localhost/bossutanthonydauphine/assets/uploads/lotus.png', 'Ma Nouvelle annonce placehold -> oiseau2', 'Ma Nouvelle annonce placehold -> Modif tout 2', 'Moi -> oiseau2', '2024-04-27 16:50:20'),
+	(62, 'https://localhost/bossutanthonydauphine/assets/uploads/oiseau.png', 'Nouvelle annonce\r\nTitre:', 'Nouvelle annonce Titre:', 'Nouvelle annonce Titre:', '2024-04-27 16:50:51');
 
--- Listage de la structure de table dauphineexamor. utilisateur
+-- Listage de la structure de table dauphineexam. utilisateur
 CREATE TABLE IF NOT EXISTS `utilisateur` (
   `id` int NOT NULL AUTO_INCREMENT,
   `username` varchar(250) NOT NULL,
@@ -51,9 +52,9 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
   UNIQUE KEY `utilisateur_login_uindex` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
--- Listage des données de la table dauphineexamor.utilisateur : ~0 rows (environ)
+-- Listage des données de la table dauphineexam.utilisateur : ~1 rows (environ)
 INSERT INTO `utilisateur` (`id`, `username`, `nom`, `prenom`, `password`) VALUES
-	(1, 'jose', 'Bové', 'Jose', '$2y$10$049yMQkSO3Jo3W4mMPYV7.Nuv9PUYEhG7tRnRnJQM7qHDyYlEyafu');
+	(1, 'jose', 'Bové', 'Jose', '$2y$10$IVM8mcb63F9YhzHEXpOZC./HT7S4oxFQVThCzX9u6av6MQvgJ9CNK');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
